f75369700b81e2041ac8511b03a357d7c79ed959
alexandrlevin <R2fJ6kL>
2018-12-04 17:51:37 +0300

    initial commit 2

--- onetwotrip-project/app/src/main/java/com/onetwotrip/alexandr/presentation/routes/tours/ToursRoute.kt
+++ onetwotrip-project/app/src/main/java/com/onetwotrip/alexandr/presentation/routes/tours/ToursRoute.kt
@@ -1,17 +1,25 @@
 package com.onetwotrip.alexandr.presentation.routes.tours
 
+import com.onetwotrip.alexandr.presentation.AppSchedulers
+import com.onetwotrip.alexandr.presentation.di.AppComponent
+import com.onetwotrip.alexandr.presentation.di.ViewScope
 import com.onetwotrip.alexandr.presentation.routes.base.MvpPresenter
 import com.onetwotrip.alexandr.presentation.routes.base.MvpView
+import dagger.Component
 import javax.inject.Inject
 
-
+@ViewScope
+@Component(dependencies = [AppComponent::class])
+interface ToursRouteComponent : AppComponent {
+    fun presenter(): ToursPresenter
+}
 
 interface ToursView : MvpView {
 
 }
 
 class ToursPresenter @Inject constructor(
-
+    private val appSchedulers: AppSchedulers
 ) : MvpPresenter<ToursView>() {
 
     fun loadData() {
